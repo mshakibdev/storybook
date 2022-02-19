9e815d8ed239227f9238e0df55698059349436ba
Md. Shakib Hassan <mshassan.dhk313@gmail.com>
2022-02-19 19:58:59 +0600

    surveyForm added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import {useCallback} from "react";
+
+import "./App.css";
+import {StylesManager, Model, Survey} from "survey-react";
+// import textJson from "./components/surveys/textQuestion";
+import radioJson from "./components/surveys/radioQuestions";
+
+StylesManager.applyTheme("modern");
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const survey = new Model(radioJson);
+	survey.focusFirstQuestionAutomatic = false;
+
+	const alertResults = useCallback((sender) => {
+		const results = JSON.stringify(sender.data);
+		alert(results);
+	}, []);
+
+	survey.onComplete.add(alertResults);
+	return (
+		<div className='App'>
+			<Survey model={survey} />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/surveys/radioQuestions.js
@@ -0,0 +1,28 @@
+const radioJson = {
+	elements: [
+		{
+			type: "imagepicker",
+			name: "choosepicture",
+			title: "What animal would you like to see first ?",
+			choices: [
+				{
+					value: "lion",
+					imageLink: "https://surveyjs.io/Content/Images/examples/image-picker/lion.jpg",
+				},
+				{
+					value: "giraffe",
+					imageLink: "https://surveyjs.io/Content/Images/examples/image-picker/giraffe.jpg",
+				},
+				{
+					value: "panda",
+					imageLink: "https://surveyjs.io/Content/Images/examples/image-picker/panda.jpg",
+				},
+				{
+					value: "camel",
+					imageLink: "https://surveyjs.io/Content/Images/examples/image-picker/camel.jpg",
+				},
+			],
+		},
+	],
+};
+export default radioJson;
